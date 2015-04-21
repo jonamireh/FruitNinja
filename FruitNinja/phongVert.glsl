88ff464903d7cdb304ce22bef7f3e719f4972dbe
petegodkin <petegodkin@gmail.com>
2015-04-20 21:10:10 -0700

    Fixed phong shader

--- FruitNinja/phongVert.glsl
+++ FruitNinja/phongVert.glsl
@@ -19,7 +19,7 @@ out vec3 vLight;
 void main()
 {
 	gl_Position = uProjMatrix * uViewMatrix * uModelMatrix * vec4(aPosition, 1.0);
-	vNormals = vec3(uViewMatrix * uModelMatrix * vec4(normalize(aNormal), 0.0));
-	vLight = uLightPos - vec3(uViewMatrix * uModelMatrix * vec4(aPosition, 1.0));
+	vNormals = vec3(uModelMatrix * vec4(normalize(aNormal), 0.0));
+	vLight = uLightPos - vec3(uModelMatrix * vec4(aPosition, 1.0));
 
 }
