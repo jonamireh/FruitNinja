b73babcc454225f0a09a3a30c249b00a45cbd567
petegodkin <petegodkin@gmail.com>
2015-05-07 14:27:29 -0700

    fixed half of switch between def and phong bug

--- FruitNinja/World.cpp
+++ FruitNinja/World.cpp
@@ -156,6 +156,15 @@ void World::draw()
 
 
 	if (usePhong) {
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LESS);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_BACK);
+		glDepthMask(GL_TRUE);
+
+
 		glUseProgram(shaders.at("simpleShader")->getProgramID());
 		glViewport(0, 0, screen_width, screen_height);
 		shaders.at("simpleShader")->draw(camera->getViewMatrix(), _skybox);
