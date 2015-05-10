d92ce9ef71dcce118d5e0e932ea4f33dc4766782
Jon Amireh <jon.amireh@gmail.com>
2015-05-10 08:27:12 -0700

    Basic guard movement

--- FruitNinja/GuardEntity.h
+++ FruitNinja/GuardEntity.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "GameEntity.h"
-#include "ChewyMovementComponent.h"
+#include "GuardMovementComponent.h"
 
 class GuardEntity : public GameEntity
 {
 public:
 	GuardEntity();
-	GuardEntity(glm::vec3 position, std::shared_ptr<MeshSet> mesh);
+	GuardEntity(glm::vec3 position, std::shared_ptr<MeshSet> mesh, float time, glm::vec3 direction);
 	void update();
+	GuardMovementComponent move_component;
 private:
 };
---
+++ FruitNinja/GuardMovementComponent.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Component.h"
+#include "GameEntity.h"
+#include "Camera.h"
+#include <glm/glm.hpp>
+#include <math.h>
+
+#define GUARD_MOVE_SPEED 10.0f
+#define GUARD_ROTATE_SPEED 15.0f
+
+class GuardMovementComponent : public Component
+{
+public:
+	GuardMovementComponent(GameEntity& entity, float time, glm::vec3 direction) : entity(entity), time(time), start_position(entity.position), direction(direction) {}
+	void update();
+	glm::vec3 direction;
+	float time;
+	glm::vec3 start_position;
+	float time_elapsed = 0.f;
+
+private:
+	GameEntity& entity;
+};
