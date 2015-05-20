1316c5746dce139927a2224c9683284af81d56f7
Jon Amireh <jon.amireh@gmail.com>
2015-05-19 22:00:14 -0700

    pathing components in proper filters

--- FruitNinja/GuardPathingComponent.cpp
+++ FruitNinja/GuardPathingComponent.cpp
@@ -14,52 +14,6 @@ GuardPathingComponent::GuardPathingComponent(vector<vec3> control_points, float 
 	change_path();
 }
 
-
-/*glm::vec3 GuardPathingComponent::getPosition(float dt)
-{
-	pair<int, float> info = getInterpolatedTime(dt);
-	int curve_index = info.first;
-	float time = info.second;
-
-	assert(time <= 1.f);
-	assert(curve_index <= control_points.size() - 2);
-
-	vector<vec3> points;
-	if (curve_index == 0)
-	{
-		points.push_back(control_points.at(0));
-		points.push_back(control_points.at(0));
-		points.push_back(control_points.at(1));
-		points.push_back(control_points.at(2));
-	}
-	else if (curve_index == control_points.size() - 2)
-	{
-		int last_index = control_points.size() - 1;
-
-		points.push_back(control_points.at(last_index - 2));
-		points.push_back(control_points.at(last_index - 1));
-		points.push_back(control_points.at(last_index));
-		points.push_back(control_points.at(last_index));
-
-	}
-	else
-	{
-		--curve_index;
-		points.push_back(control_points.at(curve_index));
-		points.push_back(control_points.at(curve_index + 1));
-		points.push_back(control_points.at(curve_index + 2));
-		points.push_back(control_points.at(curve_index + 3));
-	}
-
-	vec4 f;
-	f.x = -0.5 * pow(time, 3) + pow(time, 2) + -0.5 * time;
-	f.y = 1.5 * pow(time, 3) + -2.5 * pow(time, 2) + 1;
-	f.z = -1.5 * pow(time, 3) + 2 * pow(time, 2) + 0.5 * time;
-	f.w = 0.5 * pow(time, 3) + -0.5 * pow(time, 2);
-
-	return f.x * points.at(0) + f.y * points.at(1) + f.z * points.at(2) + f.w * points.at(3);
-}*/
-
 glm::vec3 GuardPathingComponent::getDirection()
 {
 	time_elapsed += seconds_passed;
