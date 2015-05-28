66fccfa2583e3338bcdfa08a3a333cd7ee932fdb
Ian Meeder <imeeder@cox.net>
2015-05-27 22:49:05 -0700

    Jump

--- FruitNinja/ChewyAnimationComponent.cpp
+++ FruitNinja/ChewyAnimationComponent.cpp
@@ -1,5 +1,6 @@
 #include "ChewyAnimationComponent.h"
 #include "GameEntity.h"
+#include "ChewyEntity.h"
 
 ChewyAnimationComponent::ChewyAnimationComponent(GameEntity* chewy) : basicAnimation(chewy)
 {
@@ -15,20 +16,29 @@ ChewyAnimationComponent::~ChewyAnimationComponent()
 
 void ChewyAnimationComponent::update()
 {
-	if (chewy->velocity.y > 0 && currentAnimtion != jumping)
+	if (basicAnimation.animationComplete)
 	{
-		basicAnimation.changeToSingleAnimation(JUMPING_START, JUMPING_START + JUMPING_DURATION);
-		currentAnimtion = jumping;
-	}
-	else if (chewy->moving && currentAnimtion != walking)
-	{
-		basicAnimation.changeToLoopingAnimation(WALKING_START, WALKING_START + WALKING_DURATION);
-		currentAnimtion = walking;
-	}
-	else if (!chewy->moving && currentAnimtion != standing)
-	{
-		basicAnimation.changeToLoopingAnimation(STANDING_START, STANDING_START + STANDING_DURATION);
-		currentAnimtion = standing;
+		
+		/*if (chewy->velocity.y < 15 && currentAnimtion == jumping && currentAnimtion)
+		{
+			basicAnimation.changeToSingleAnimation(FALLING_START, FALLING_START + FALLING_DURATION);
+			currentAnimtion = falling;
+		}
+		else */if (chewy->velocity.y > 0 && currentAnimtion != jumping && currentAnimtion != falling)
+		{
+			basicAnimation.changeToSingleAnimation(JUMPING_START, JUMPING_START + JUMPING_DURATION);
+			currentAnimtion = jumping;
+		}
+		else if (chewy->velocity.y == 0 && chewy->moving && currentAnimtion != walking)
+		{
+			basicAnimation.changeToLoopingAnimation(WALKING_START, WALKING_START + WALKING_DURATION);
+			currentAnimtion = walking;
+		}
+		else if (chewy->velocity.y == 0 && !chewy->moving && currentAnimtion != standing)
+		{
+			basicAnimation.changeToLoopingAnimation(STANDING_START, STANDING_START + STANDING_DURATION);
+			currentAnimtion = standing;
+		}
 	}
 	basicAnimation.update();
 }
