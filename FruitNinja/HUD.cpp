91a689b498f91b1fa373555b455a8a5c934128b4
petegodkin <petegodkin@gmail.com>
2015-05-28 17:56:14 -0700

    Enable blend for HUD

--- FruitNinja/HUD.cpp
+++ FruitNinja/HUD.cpp
@@ -26,10 +26,12 @@ HUD::~HUD()
 
 void HUD::draw()
 {
+	glEnable(GL_BLEND);
 	shader.drawTexture(arrow_hud->object(), screen_width - arrow_hud->originalWidth() * .5 - 20, screen_height - arrow_hud->originalHeight() * .5 - 20, arrow_hud->originalWidth() * .5, arrow_hud->originalHeight() * .5);
 	if (chewy->moveComponent.archery_cam->in_use)
 	{
 		shader.drawTexture(power_bar_bar->object(), screen_width - power_bar_frame->originalWidth() - 20, screen_height - 40 - arrow_hud->originalHeight() * .5 - power_bar_frame->originalHeight(), power_bar_frame->originalWidth(), power_bar_frame->originalHeight(), bow_strength);
 		shader.drawTexture(power_bar_frame->object(), screen_width - power_bar_frame->originalWidth() - 20, screen_height - 40 - arrow_hud->originalHeight() * .5 - power_bar_frame->originalHeight(), power_bar_frame->originalWidth(), power_bar_frame->originalHeight());
 	}
+	glDisable(GL_BLEND);
 }
