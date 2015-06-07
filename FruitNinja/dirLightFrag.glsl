7f55c478a9b0fce0f02812da49813bdae36c3a63
Keenan Swafford <keenanswafford@gmail.com>
2015-06-07 04:42:21 -0700

    Cleaned up dirLightShader; made directional light color more blue

--- FruitNinja/dirLightFrag.glsl
+++ FruitNinja/dirLightFrag.glsl
@@ -24,21 +24,6 @@ out vec4 FragColor;
   vec2( 0.34495938, 0.29387760 )
 );*/
 
-/*vec2 poissonDisk[12] = vec2[](
-	vec2(-0.5560945f, -0.1039571f),
-	vec2(-0.03921358f, 0.2479071f),
-	vec2(0.3701732f, -0.06092868f),
-	vec2(-0.7933782f, 0.5168048f),
-	vec2(-0.2650119f, -0.7896724f),
-	vec2(-0.07208196f, 0.7526851f),
-	vec2(0.595312f, 0.4872388f),
-	vec2(0.7884521f, -0.5025175f),
-	vec2(0.2916443f, -0.6768091f),
-	vec2(0.9375908f, 0.003824265f),
-	vec2(-0.06717162f, -0.3103488f),
-	vec2(-0.750717f, -0.5788005f)
-);*/
-
 /*vec2 poissonDisk[16] = vec2[](
 	vec2(0.6516818f, 0.6328417f),
 	vec2(0.1121163f, 0.1513577f),
@@ -83,23 +68,9 @@ float calcShadowFactor(vec3 pos, float cosTheta)
 	float visibility = 1.0;
 	for (int i = 0; i < 4; i++) {
 		//int index = int(16.0 * random(pos, i)) % 16;
-		//float depth = texture(shadowMap, uvCoords + poissonDisk[index] / 2000.0).x;
-		//if (depth < (z - bias)) {
-		//	visibility -= (1.0 / 5.0);
-		//}
+		//visibility -= (1.0 / 5.0) * (1.0 - texture(shadowMap, vec3(uvCoords + poissonDisk[index] / 2000.0, z)));
 
-		//int index = int(4.0 * random(pos, i)) % 4;
-		//visibility -= 0.4 * (1.0 - texture(shadowMap, uvCoords + poissonDisk[index] / 700.0).x);
-		//visibility = max(0, visibility);
-
-		//int index = int(12.0 * random(pos, i)) % 12;
-		//visibility -= (1.0 / 5.0) * (1.0 - texture(shadowMap, uvCoords + poissonDisk[index] / 700.0).x);
-		//visibility = max(0, visibility);
-
-		//float depth = texture(shadowMap, uvCoords + poissonDisk[i] / 1000.0).x;
-		//if (depth < (z + 0.00001)) {
-		//	visibility -= (1.0 / 5.0);
-		//}
+		//visibility -= (1.0 / 5.0) * (1.0 - texture(shadowMap, vec3(uvCoords + poissonDisk[i] / 2000.0, z)));
 	}
 
 	return visibility;*/
@@ -108,7 +79,7 @@ float calcShadowFactor(vec3 pos, float cosTheta)
 vec4 calcLightInternal(vec3 lightDir, vec3 worldPos, vec3 normal)
 {
 	//white light
-	vec3 color = vec3(1.0, 1.0, 1.0);
+	vec3 color = vec3(0.15, 0.4, 1.0);
     vec4 ambientColor = vec4(color, 1.0) * AMBIENT_INTENSITY;
     float diffuseFactor = dot(normal, -lightDir);
 
