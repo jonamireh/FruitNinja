db4a48650fe600dc3bea4e7a30eb498d4da0db69
Keenan Swafford <keenanswafford@gmail.com>
2015-06-07 14:35:59 -0700

    Removed flame sound for lanterns

--- FruitNinja/LightEntity.h
+++ FruitNinja/LightEntity.h
@@ -7,27 +7,21 @@
 
 class LightEntity : public GameEntity
 {
-	FMOD::Channel *flame_channel;
 public:
     LightEntity() : light(NULL)
 	{
         collision_response = true;
-		flame_channel = AudioManager::instance()->play3D(assetPath + "flame.wav", light->pos, 0.2f, true);
 	}
 	//color will default to white, probably use this
 	LightEntity(glm::vec3 position, MeshSet* mesh, float intensity, MeshSet* unit_sphere)
 		: GameEntity(position, mesh), light(new Light(getPosition(), glm::vec3(1.0, 1.0, 1.0), intensity, unit_sphere))
 	{
-		flame_channel = AudioManager::instance()->play3D(assetPath + "flame.wav", light->pos, 0.2f, true);
 	}
 
 	LightEntity(glm::vec3 position, MeshSet* mesh, float intensity, MeshSet* unit_sphere, glm::vec3 color)
 		: GameEntity(position, mesh), light(new Light(getPosition(), color, intensity, unit_sphere))
 	{
-		flame_channel = AudioManager::instance()->play3D(assetPath + "flame.wav", light->pos, 0.2f, true);
 	}
 	void update();
 	Light* light;
-
-	void disableFlameSound();
 };
